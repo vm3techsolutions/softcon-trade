c73ef209b3a42eb7e9a90e84c535c25154f14b60
Vishal Bhange <vishalbhange333@gmail.com>
2025-05-29 18:00:17 +0530

    UI

--- frontend/src/app/categories/IECCubic.js
+++ frontend/src/app/categories/IECCubic.js
@@ -1,7 +1,7 @@
 export default function Rockwell() {
   return (
     <div>
-      <h2 className="text-2xl font-semibold text-[#044E78] mb-4"> Automation</h2>
+      <h2 className="text-2xl font-semibold text-[#044E78] mb-4"> IEC-61439 Cubic Panels</h2>
       <p>This is the detailed content for Rockwell Automation.</p>
     </div>
   );
--- frontend/src/app/categories/Vacon.js
+++ frontend/src/app/categories/Vacon.js
@@ -1,7 +1,7 @@
 export default function Rockwell() {
   return (
     <div>
-      <h2 className="text-2xl font-semibold text-[#044E78] mb-4">Rockwell Automation</h2>
+      <h2 className="text-2xl font-semibold text-[#044E78] mb-4"> Vacon</h2>
       <p>This is the detailed content for Rockwell Automation.</p>
     </div>
   );
