a71e757e91bb7c83c9abdc401a7fe7d6eb554875
RutikGaikwad123 <ruttikgaikwad@gmail.com>
2025-06-12 17:53:16 +0530

    Admin register , login backend api's creation

--- backend/routes/auth-router.js
+++ backend/routes/auth-router.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const verifyToken = require('../middleware/auth');
 const userRegister = require('../controller/userRegister/register');
 const userGetData = require('../controller/userGetData/userGetData');
+const adminRegister = require('../controller/adminRegister/adminRegister');
 
 
 // Define routes for user registration and login
@@ -11,6 +12,11 @@ router.post('/user/signup', userRegister.userSignUp);
 router.post('/user/login', userRegister.userLogin);
 // Define route for getting user data
 router.get('/user/getData/:id', verifyToken, userGetData.userGetData);
+
+// Admin routes
+router.post('/admin/signup', adminRegister.adminSignUp);
+router.post('/admin/login', adminRegister.adminLogin);
+router.get('/admin/getData/:id', verifyToken, adminRegister.getAdminData);
 
 
 module.exports = router;
