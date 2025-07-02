f87acdb6527d684a4c0fb6bec1ace716edecc53d
poojaraut2618 <poojaraut2618@gmail.com>
2025-07-02 22:19:56 +0530

    update api url in frontend

--- frontend/src/app/api/axiosInstance.js
+++ frontend/src/app/api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:5000",
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
